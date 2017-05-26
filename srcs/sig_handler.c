a2d2d70bd50581343c8f42b3bec64cf31431192d
Foo <unknow>
2017-05-26 16:14:09 +0200

    fix ctrl+c crop upper line on multiline wihout \n

--- srcs/sig_handler.c
+++ srcs/sig_handler.c
@@ -2,26 +2,7 @@
 #include "read_input/editor/editor.h"
 #include "errno.h"
 #include <sys/wait.h>
-#include <stdio.h>
-
 #include "read_input/event_callbacks/event_callback_def.h"
-
-// void		sig_int(int sig)
-// {
-// 	t_editor	*ed;
-
-// 	(void)sig;
-// 	ed = get_editor();
-// 	kill(-2, SIGKILL);
-// 	if (ed->in_edition == true)
-// 	{
-// 		ev_cursor_end(ed);
-// 		change_string(ed, "");
-// 		fflush(stdout);
-// 		ft_putstr("^C\n");
-// 		put_line(ed);
-// 	}
-// }
 
 void		sig_int(int sig)
 {
@@ -32,12 +13,21 @@ void		sig_int(int sig)
 	ed = get_editor();
 	if (ed->in_edition)
 	{
+		
+		// Need quit quote prompt //
+
 		if (get_shell_env()->last_unmatched)
 			get_shell_env()->del_input = 1;
 		get_shell_env()->last_unmatched = NO_ERROR;
+		ev_cursor_end(ed);
+		ft_putstr("^C\n");
 		change_string(ed, "");
-		ft_putchar('\n');
+		ed->pos = get_cursor_vector(ed);
 		ed->need_refresh = true;
 		refresh_line(ed);
+	}
+	else
+	{
+		// ft_putstr("\n");
 	}
 }
